e27c8862e6dd64224208d7d7cc88034ed9e2a33b
Nikita Endea <90311455+Nikita-Endeka2004@users.noreply.github.com>
2022-11-04 17:56:55 +0200

    jsStart

---
+++ assets/js/app.js
@@ -0,0 +1,103 @@
+
+import { createApp } from "../../node_modules/vue/dist/vue.esm-browser.prod.js";
+
+    createApp = ({
+        data(){
+            return{
+
+                inn: '',
+                sex: {check: false, gender: 'Женьщина'},
+                birthday: new Date(1900, 0, 0),
+                age: 0,
+                trueInn: false
+
+            }
+        },
+        methods:{
+
+            forInput(){
+
+                let num = this.inn.trim();
+                num = num.split('');
+                let DateBirthdayInNumber = '';
+                let dateToday = new Date();
+
+                if((num[8] == 8) || (num[8] == 1) || ((num[8] % 2) == 0)){
+
+                    this.sex.map(item => item.cheak = false);
+
+                    for (let i = 0; i < 5; i++) {
+                        DateBirthdayInNumber = DateBirthdayInNumber + num[i];
+                    }
+
+                    DateBirthdayInNumber = DateBirthdayInNumber / 1;
+                    this.birthday.setDate( this.birthday.getDate() + DateBirthdayInNumber);
+                    this.age = dateToday.getFullYear() - this.birthday.getFullYear();
+                }else{
+
+                    this.sex.map(item => item.cheak = true);
+        
+                    for (let i = 0; i < 5; i++) {
+                        DateBirthdayInNumber = DateBirthdayInNumber + num[i];
+                    }
+        
+                    DateBirthdayInNumber = DateBirthdayInNumber / 1;
+                    this.birthday.setDate( this.birthday.getDate() + DateBirthdayInNumber);
+                    this.age = dateToday.getFullYear() - this.birthday.getFullYear();
+                }
+
+            },
+            forSex(){
+
+                let stat = this.sex.check;
+                if(stat){
+                    this.sex.map(item => item.gender = "Мужчина");
+                }else{
+                    this.sex.map(item => item.gender = "Женьщина");
+                }
+
+            },
+            forCorrectAge(){
+
+                if(this.birthday.getMonth() <= this.dateToday.getMonth()){
+                    if(this.birthday.getDate() <= this.dateToday.getDate()){
+                        this.age = this.age;
+                    }else{
+                        this.age = this.age - 1;
+                    }
+    
+                }
+                if(this.birthday.getMonth() > this.dateToday.getMonth()){
+                    if(this.birthday.getDate() > this.dateToday.getDate()){
+                        this.age = this.age - 1;
+                    }else{
+                        this.age = this.age - 1;
+                    }
+                }
+
+            },
+            forCheckCorrectInn(){
+
+                let checkSum;
+                let number = this.inn.trim();
+                number = number.split('');
+                checkSum = ((
+                    2 * number[0] +
+                    4 * number[1] +
+                    10 * number[2] +
+                    3 * number[3] +
+                    5 * number[4] +
+                    9 * number[5] +
+                    4 * number[6] +
+                    6 * number[7] +
+                    8 * number[8]
+                ) % 11 ) % 10;
+                if(checkSum === Number(number[9])){
+                    this.trueInn = true;
+                }else{
+                    this.trueInn = false;
+                }
+
+            }
+        }
+    }).mount('#app');
